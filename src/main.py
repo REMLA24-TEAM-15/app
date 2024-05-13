ac5c85eec95fc4d42261264e7e79d5fd05994cdc
Michal Raczkiewicz <m.raczkiewicz@student.tudelft.nl>
2024-05-13 09:41:51 +0200

    initial release workflow test

--- src/main.py
+++ src/main.py
@@ -1,24 +1,28 @@
 from flask import Flask, request, render_template, jsonify
 import requests
+import os
 from lib_version_URLPhishing.version_util import VersionUtil
 
 app = Flask(__name__)
 
-@app.route('/', methods =['GET'])
+@app.route("/", methods =["GET"])
 def index ():
-	return render_template('index.html')
+	return render_template("index.html")
 
 @app.route("/query", methods=["POST"])
 def query():
-	uri = request.json.get("uri")
-	url = "http://model-service:8081/predict"
-	params = {"URI": uri}
-	response = requests.post(url, params=params)
+	endpoint_uri = os.environ.get("MODEL_SERVICE_URI", "http://localhost:8081/")
+	endpoint_uri = endpoint_uri + "predict"
+
+	uri_to_check = request.json.get("uri")
+	
+	params = {"link": uri_to_check}
+	response = requests.post(uri_to_check, params=params)
 
 	if response.status_code == 200:
 		data = response.json()
-		is_phishing = data.get("is_phishing")
-		return jsonify({"is_phishing": is_phishing})
+		prediction = data.get("Prediction")
+		return jsonify({"Prediction": prediction})
 	else:
 		print("Error:", response.status_code)
 		return None
