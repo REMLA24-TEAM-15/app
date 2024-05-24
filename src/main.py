b0919d01002740c0298b5b7b7d6d430c38093f39
Michal Raczkiewicz <m.raczkiewicz@student.tudelft.nl>
2024-05-24 16:45:24 +0200

    added guard to flask so that pdoc does not trigger it

--- src/main.py
+++ src/main.py
@@ -70,4 +70,5 @@ def get_version():
     return jsonify({"version": version})
 
 
-app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
